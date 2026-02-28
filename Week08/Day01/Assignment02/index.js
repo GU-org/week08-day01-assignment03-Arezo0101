39e6007db08735cb8b31e2aaf8dbd789d4fd4ec9
arezoahmadi <arezoahmadiarezo1@gmail.com>
2026-02-28 10:03:34 +0430

    Done

--- Week08/Day01/Assignment02/index.js
+++ Week08/Day01/Assignment02/index.js
@@ -3,12 +3,13 @@ function lifeInWeeks(age) {
 /************Don't change the code above************/    
     
     //Write your code here.
-   
-    
-    
-    
-    
-    
+  
+  var yearsLeft = 90 - age;
+  var days = yearsLeft * 365;
+  var weeks = yearsLeft * 52;
+  var months = yearsLeft * 12;
+
+  console.log("You have " + days + " days, " + weeks + " weeks, and " + months + " months left.");
+ 
 /*************Don't change the code below**********/
 }
-
